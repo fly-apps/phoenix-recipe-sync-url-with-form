e38118d921d1bbac581a4ab00e9d452b047fa155
lubien <lubien1996@gmail.com>
2022-11-10 08:20:35 -0300

    Create PageLive

---
+++ lib/form_url_recipe_web/live/page_live.ex
@@ -0,0 +1,11 @@
+defmodule FormUrlRecipeWeb.PageLive do
+  use FormUrlRecipeWeb, :live_view
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      A
+    </div>
+    """
+  end
+end
--- lib/form_url_recipe_web/router.ex
+++ lib/form_url_recipe_web/router.ex
@@ -17,7 +17,7 @@ defmodule FormUrlRecipeWeb.Router do
   scope "/", FormUrlRecipeWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    live "/", PageLive, :index
   end
 
   # Other scopes may use custom stacks.
